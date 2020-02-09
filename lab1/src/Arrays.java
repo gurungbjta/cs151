3bb06b31e7fcb5e0028affff7c8fe7b2ed767527
Bijayata Gurung <gurungbjta@Bijayatas-MacBook.local>
2020-02-08 16:03:32 -0800

    lab1

--- lab1/src/Arrays.java
+++ lab1/src/Arrays.java
@@ -1,21 +1,19 @@
-// Implement the method Arrays.swapLargestAndSmallest(int[] a) that swaps the largest and smallest element in a.
-// If there are multiple largest or smallest elements, pick the first one.
 public class Arrays {
     public static void swapLargestAndSmallest(int[] a) {
-         int smallestIndex = 0;
-         int largestIndex = 0;
+        int smallestIndex = 0; // suppose the smallest element is at index 0
+        int largestIndex = 0; // suppose the largest element is at index 0
 
-         for (int i = 1; i < a.length; i++){
-             if (a[i] < a[smallestIndex]){
-                 smallestIndex = i;
-             }
-             if (a[i] > a[largestIndex]){
-                 largestIndex = i;
-             }
-         }
-         int temp = a[smallestIndex];
-         a[smallestIndex] = a[largestIndex];
-         a[largestIndex] = temp;
+        for (int i = 1; i < a.length; i++){
+            if (a[i] < a[smallestIndex]){ // if the element in the array is smaller than the element in the smallestIndex, swap index
+                smallestIndex = i;
+            }
+            if (a[i] > a[largestIndex]){ // if the element in the array is larger than the element in the largestIndex, swap index
+                largestIndex = i;
+            }
+        }
+        int temp = a[smallestIndex]; // creating a temp place for smallest element
+        a[smallestIndex] = a[largestIndex];
+        a[largestIndex] = temp;
 
     }
 }
